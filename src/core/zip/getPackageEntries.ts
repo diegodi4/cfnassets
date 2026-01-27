76e521eb6b9cde08246c8be928ebe3e4d657c8dc
diegodi4 <diquattro@gmail.com>
2026-01-27 16:34:09 +0000

    fix: add --no-frozen-lockfile to pnpm install

--- src/core/zip/getPackageEntries.ts
+++ src/core/zip/getPackageEntries.ts
@@ -34,7 +34,7 @@ export async function* getPackageEntries({
   } else if (lockBasename === 'yarn.lock') {
     exec = ['yarn', '--frozen-lockfile'];
   } else if (lockBasename === 'pnpm-lock.yaml') {
-    exec = ['pnpm', 'install'];
+    exec = ['pnpm', 'install', '--no-frozen-lockfile'];
   } else {
     throw new Error(`unknown lockfile type for path '${packageLockPath}'`);
   }
